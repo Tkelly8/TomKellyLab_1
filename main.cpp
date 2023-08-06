27cc9f01c26bdee249af2546d84b70ce394c51b6
Tkelly8 <edenaun@gmail.com>
2023-08-06 18:49:02 +0000

    program working correctly
    Signed-off-by: Tkelly8 <edenaun@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 // main() runs in its own thread in the OS
 InterruptIn bttn(PC_13);
+char flag0 = 0;  //flag variable set to 0
 
 void bttn_interrupt(){
+    flag0 = 1;   //flag varible set to 1 when he voltage falls to 0 volts 
+
     //printf("Buttn pressed \r\n");
     //causes following error
     //Error Message: Mutex: 0x200015FC, Not allowed in ISR context
@@ -11,14 +14,23 @@ void bttn_interrupt(){
     // will have to use flags or EventQueue to get it to run
 
 }
-int main ()
-{
-    bttn.rise(&bttn_interrupt );
-    printf("program ready to start\r\n");
-    while (true) {
-        printf("Buttn pressed \r\n");
-        break;
+int main (){
 
+    bttn.fall(&bttn_interrupt );  //pressing button triggers interrupt, no need to release it
+    
+    printf("program ready to start\r\n");
+     while (true) {
+        switch (flag0){
+            case 1:   //if flag is 1 then prints button is pressed
+                printf("Blue Button pressed \r\n");
+                flag0 = 0;   //reset flag to 0
+                break;
+        
+            default:
+                flag0 = 0;
+                break;
+
+        }
+    ThisThread :: sleep_for(10ms);  //ensures button bounce does no cause the mbed to see two presses
     }
 }
-

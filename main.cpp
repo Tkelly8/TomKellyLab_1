93d361bf4e27f088197f5f3c19d76fb99fd45a04
Tkelly8 <edenaun@gmail.com>
2023-08-06 19:03:09 +0000

    code improved by having flags set for press and let go
    Signed-off-by: Tkelly8 <edenaun@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ InterruptIn bttn(PC_13);
 char flag0 = 0;  //flag variable set to 0
 
 void bttn_interrupt(){
-    flag0 = 1;   //flag varible set to 1 when he voltage falls to 0 volts 
+    flag0 = 1;   //flag 1 varible set to 1 when he voltage falls to 0 volts 
 
     //printf("Buttn pressed \r\n");
     //causes following error
@@ -14,15 +14,25 @@ void bttn_interrupt(){
     // will have to use flags or EventQueue to get it to run
 
 }
+void bttn_interrupt_off(){
+    flag0 = 2;   //flag 2 varible set to 2 when he voltage falls to 0 volts
+}
+
 int main (){
 
     bttn.fall(&bttn_interrupt );  //pressing button triggers interrupt, no need to release it
-    
+    bttn.rise(&bttn_interrupt_off );  //letting go of button
+
     printf("program ready to start\r\n");
-     while (true) {
+    while (true) {
         switch (flag0){
             case 1:   //if flag is 1 then prints button is pressed
                 printf("Blue Button pressed \r\n");
+                flag0 = 0;   //reset flag to 0
+                break;
+                
+            case 2:   //if flag is 2 then prints button is let go
+                printf("Blue Button let go \r\n");
                 flag0 = 0;   //reset flag to 0
                 break;
         
@@ -31,6 +41,6 @@ int main (){
                 break;
 
         }
-    ThisThread :: sleep_for(10ms);  //ensures button bounce does no cause the mbed to see two presses
+    ThisThread :: sleep_for(10ms);  //ensures button bounce does not cause the mbed to see two presses
     }
 }

0d27272e4d7b23cea746696c994ddbc6fdf9b6c2
Tkelly8 <edenaun@gmail.com>
2023-08-06 18:39:39 +0000

    error removed but not funtioning yet
    Signed-off-by: Tkelly8 <edenaun@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@
 InterruptIn bttn(PC_13);
 
 void bttn_interrupt(){
-    printf("Buttn pressed \r\n");
+    //printf("Buttn pressed \r\n");
     //causes following error
     //Error Message: Mutex: 0x200015FC, Not allowed in ISR context
-    // the print funtion cannot be ran in this area of the program
+    // the print funtion cannot be ran in this area of the p rogram because memory isn't piriotised for it.
+    // will have to use flags or EventQueue to get it to run
 
 }
 int main ()
 {
-    bttn.fall(&bttn_interrupt );
+    bttn.rise(&bttn_interrupt );
+    printf("program ready to start\r\n");
     while (true) {
+        printf("Buttn pressed \r\n");
+        break;
 
     }
 }
